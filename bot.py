516cc93ce529fd96e2f4268a26649ff12486bf8f
rucbar <rucbar1998@gmail.com>
2020-01-17 18:03:32 -0500

    Groovy Command remove/DM scolding

--- bot.py
+++ bot.py
@@ -17,30 +17,47 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-
+        #If connected to my guild
         for GUILD in client.guilds:
                 if GUILD == GUILD.name:
                         break
+        #prints guild
         print(
                 f'\n\n{client.user} is connected to the following GUILD:\n'
                 f'{GUILD.name}(id: {GUILD.id})\n'
         )
-
-        members = '\n - '.join([member.name for member in GUILD.members])
-        print(f'GUILD Members:\n - {members}')
+        #Lists the members in my server
+        #members = '\n - '.join([member.name for member in GUILD.members])
+        #print(f'GUILD Members:\n - {members}')
 
 @client.event
 async def on_message(m):
+        print("MESSAGE FROM " + str(m.author))
+        radioChannel = "radio-shit"
+        radioName = "Groovy"
+        commandChar = '-'
+        wrongChannelMsg = "HEY!!! That's the wrong channel you tard... Go to '" + radioChannel + "'"
 
-        if m.content.startswith('-'):
+        #If commandChar and in the wrong channel
+        if m.content.startswith(commandChar) and str(m.channel) != radioChannel:
                 try:
-                        await m.delete(delay=1)
-                except Forbidden:
-                        print("No permissions to delete file")
-                except HTTPException:
+                        #DM user
+                        await m.author.send(content=wrongChannelMsg)
+                        #Delete message (delay=Optional Delay seconds)
+                        await m.delete(delay=0)
+                except:
                         print("Deleting has failed for unknown reason")
                 else:
-                        print("One Message Deleted that begins with '-'")
+                        print("Command message deleted")           
+
+        if m.author.name == radioName and str(m.channel) != radioChannel:
+                try:
+                        await m.delete(delay=0)
+                except:
+                        print("Deleting failed for unknown reason")
+                else:
+                        print("Groovy message deleted")
+
 
 
 
