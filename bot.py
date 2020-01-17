0a593538fe3834cc8b32b46e3bcaa73be26e88be
rucbar <rucbar1998@gmail.com>
2020-01-16 22:28:10 -0500

    Deletes -queue from groovy but nothing else

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import time
+import asyncio
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
-guild = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+GUILDID = os.getenv('DISCORD_GUILD_ID')
 
 
 client = discord.Client()
@@ -15,16 +18,38 @@ client = discord.Client()
 @client.event
 async def on_ready():
 
-        for guild in client.guilds:
-                if guild == guild.name:
+        for GUILD in client.guilds:
+                if GUILD == GUILD.name:
                         break
         print(
-                f'\n\n{client.user} is connected to the following guild:\n'
-                f'{guild.name}(id: {guild.id})\n'
+                f'\n\n{client.user} is connected to the following GUILD:\n'
+                f'{GUILD.name}(id: {GUILD.id})\n'
         )
 
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
+        members = '\n - '.join([member.name for member in GUILD.members])
+        print(f'GUILD Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+        #Is user message a command?
+        if (message.content.startswith("-")):
+                await asyncio.sleep(1)
+                message.delete
+        
+        #find Groovy user
+        for GUILD in client.guilds:
+                if GUILD == GUILD.name:
+                        break
+        for member in GUILD.members:
+                if member.name == "Groovy":
+                        groovyUser = member
+                        break
+
+        def is_bot(message):
+                return message.author == groovyUser
+        
+        await message.channel.purge(limit=100, check=is_bot)
 
 
-client.run(token)
+
+client.run(TOKEN)
